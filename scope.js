ec7f7531f0a68eb5e77820095370ce76bc6204ba
Faysal Khan <mdfaysalkhancse@gmail.com>
2021-02-03 00:26:21 +0600

    slice method practice

---
+++ scope.js
@@ -0,0 +1,19 @@
+
+let bonus = 20;
+function add(first, second){
+    let result = first + second + bonus;
+    // console.log(bonus);
+    console.log(result);
+    if(result > 9){
+         mood = 'happy';
+        mood = 'ranky';
+        mood = 'banky';
+        console.log(mood);
+    }
+    return result;
+
+}
+const output = add(4,6);
+
+console.log(output);
+
