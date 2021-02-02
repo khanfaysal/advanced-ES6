a64ceebca7a56763940c99ade75327634f6947f4
Faysal Khan <mdfaysalkhancse@gmail.com>
2021-02-02 13:22:45 +0600

    double and tripple equal comparision practice done

---
+++ double-vs-tripple-equal.js
@@ -0,0 +1,17 @@
+/* const firstNumber = 3;
+const secondNumber = '3';
+if(firstNumber == secondNumber){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+} */
+
+const firstNumber = 1;
+const secondNumber = true;
+if(firstNumber === secondNumber){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
