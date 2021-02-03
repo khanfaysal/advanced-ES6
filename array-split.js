8187e6fa8a408285243fc42c2e5f77a48197ce0f
Faysal Khan <mdfaysalkhancse@gmail.com>
2021-02-03 11:18:08 +0600

    break-continue

---
+++ array-split.js
@@ -0,0 +1,7 @@
+let numbers = [1,2,3,4,5,6,7,8];
+ var result = numbers.slice(1,3);
+// const result = numbers.splice(-1,5,10,11,12,13);
+const join = result.join('ami')
+// console.log(numbers);
+// console.log(result);
+console.log(join);
